c1fb793f1b40ed75be76ff210aaf8ee26ed6b6e9
Aditi Gurav <aditi.gurav@cumminscollege.in>
2026-02-05 15:47:52 +0530

    This is for deleting result

--- DeleteResult.java
+++ DeleteResult.java
@@ -1,22 +1,24 @@
 import java.util.Iterator;
-import java.util.Scanner;
 
 public class DeleteResult {
 
-    public static void delete() {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter Roll Number to delete: ");
-        int roll = sc.nextInt();
+    public static boolean delete(int roll) {
+
+        // 👇 ADD THIS DEBUG LINE
+        System.out.println("LIST SIZE BEFORE DELETE = " + DataStore.students.size());
 
         Iterator<Student> itr = DataStore.students.iterator();
+
         while (itr.hasNext()) {
             Student s = itr.next();
             if (s.roll == roll) {
                 itr.remove();
-                System.out.println("Result deleted successfully\n");
-                return;
+                System.out.println("DELETED");
+                return true;
             }
         }
-        System.out.println("Result not found\n");
+
+        System.out.println("NOT FOUND");
+        return false;
     }
 }
---
+++ Student.java
@@ -0,0 +1,9 @@
+public class Student {
+    public int roll;
+    public String name;
+
+    public Student(int roll, String name) {
+        this.roll = roll;
+        this.name = name;
+    }
+}
