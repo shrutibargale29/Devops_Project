0e1e2a696f0a97fba1ca3d5762332dca9917b525
Aditi Gurav <aditi.gurav@cumminscollege.in>
2026-04-23 10:38:00 +0530

    maven completed

---
+++ src/main/java/com/example/Student.java
@@ -0,0 +1,10 @@
+package com.example;
+public class Student {
+    public int roll;
+    public String name;
+
+    public Student(int roll, String name) {
+        this.roll = roll;
+        this.name = name;
+    }
+}
